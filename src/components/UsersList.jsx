c321b8ab5e5f181247dfd985a7ebac396df000b7
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2024-01-27 08:08:26 +0100

    configure the users slice and state

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from '../store';
 
 const UsersList = () => {
-  return <div>UsersList</div>;
+  const { isLoading, data, error } = useSelector((state) => state.users);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error Fetching Data...</div>;
+  }
+  return <div>{data.length}</div>;
 };
 
 export default UsersList;
---
+++ src/store/thunks/fetchUsers.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const fetchUsers = createAsyncThunk('users/fetch', async () => {
+  const response = await axios.get('http://localhost:3000/users');
+
+  //Dev only
+  await pause(1000);
+  return response.data;
+});
+
+// Dev Only
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
+
+export { fetchUsers };
